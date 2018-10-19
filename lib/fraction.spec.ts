c3f23df8e5b92b76ae7ce0ea304d714cf9ff5f17
Ian Johnson <ianprime0509@gmail.com>
2018-10-19 14:04:58 -0400

    Split up some test cases

--- lib/fraction.spec.ts
+++ lib/fraction.spec.ts
@@ -10,10 +10,16 @@ import 'mocha';
 import { Fraction } from './fraction';
 
 describe('constructor', () => {
-  it('constructs a fraction in lowest terms', () => {
-    expect(new Fraction(2, 3)).to.deep.equal(new Fraction(2, 3));
-    expect(new Fraction(6, 8)).to.deep.equal(new Fraction(3, 4));
-    expect(new Fraction(2, 5)).not.to.deep.equal(new Fraction(1, 4));
+  it('constructs a fraction given a numerator and denominator', () => {
+    const f = new Fraction(2, 3);
+    expect(f.numerator).to.equal(2);
+    expect(f.denominator).to.equal(3);
+  });
+
+  it('reduces numerator and denominator to lowest terms', () => {
+    const f = new Fraction(6, 8);
+    expect(f.numerator).to.equal(3);
+    expect(f.denominator).to.equal(4);
   });
 });
 
@@ -22,9 +28,11 @@ describe('fromMixedNumber', () => {
     expect(Fraction.fromMixedNumber(1, new Fraction(1, 3))).to.deep.equal(
       new Fraction(4, 3),
     );
+
     expect(Fraction.fromMixedNumber(0, new Fraction(5, 6))).to.deep.equal(
       new Fraction(5, 6),
     );
+
     expect(Fraction.fromMixedNumber(4, new Fraction(2, 3))).to.deep.equal(
       new Fraction(14, 3),
     );
@@ -32,15 +40,19 @@ describe('fromMixedNumber', () => {
 });
 
 describe('toMixedNumber', () => {
-  it('converts an improper fraction into a mixed number', () => {
+  it('converts a proper fraction into a mixed number', () => {
     expect(new Fraction(3, 4).toMixedNumber()).to.deep.equal([
       0,
       new Fraction(3, 4),
     ]);
+  });
+
+  it('converts an improper fraction into a mixed number', () => {
     expect(new Fraction(3, 2).toMixedNumber()).to.deep.equal([
       1,
       new Fraction(1, 2),
     ]);
+
     expect(new Fraction(15, 4).toMixedNumber()).to.deep.equal([
       3,
       new Fraction(3, 4),
--- lib/ingredient.spec.ts
+++ lib/ingredient.spec.ts
@@ -15,9 +15,11 @@ describe('parse', () => {
     expect(Ingredient.parse('1 cup flour')).to.deep.equal(
       new Ingredient(1, 'cup', 'flour'),
     );
+
     expect(Ingredient.parse('4 cups baking soda')).to.deep.equal(
       new Ingredient(4, 'cups', 'baking soda'),
     );
+
     expect(Ingredient.parse('3 teaspoons crushed red pepper')).to.deep.equal(
       new Ingredient(3, 'teaspoons', 'crushed red pepper'),
     );
@@ -27,9 +29,11 @@ describe('parse', () => {
     expect(Ingredient.parse('1/2 cup oats')).to.deep.equal(
       new Ingredient(new Fraction(1, 2), 'cup', 'oats'),
     );
+
     expect(Ingredient.parse('2 / 3 cup powdered sugar')).to.deep.equal(
       new Ingredient(new Fraction(2, 3), 'cup', 'powdered sugar'),
     );
+
     expect(Ingredient.parse('5/6 teaspoons baking powder')).to.deep.equal(
       new Ingredient(new Fraction(5, 6), 'teaspoons', 'baking powder'),
     );
@@ -39,15 +43,17 @@ describe('parse', () => {
     expect(Ingredient.parse('1 1/2 cups oat bran')).to.deep.equal(
       new Ingredient(new Fraction(3, 2), 'cups', 'oat bran'),
     );
+
     expect(Ingredient.parse('3 3 / 4 cups warm water')).to.deep.equal(
       new Ingredient(new Fraction(15, 4), 'cups', 'warm water'),
     );
   });
 
-  it('parses preparation instructions', () => {
+  it('parses ingredients with a single preparation instruction', () => {
     expect(Ingredient.parse('1 cup red onions, minced')).to.deep.equal(
       new Ingredient(1, 'cup', 'red onions', ['minced']),
     );
+
     expect(
       Ingredient.parse('1 1/4 teaspoon red pepper, finely chopped'),
     ).to.deep.equal(
@@ -55,6 +61,9 @@ describe('parse', () => {
         'finely chopped',
       ]),
     );
+  });
+
+  it('parses ingredients with multiple preparation instructions', () => {
     expect(
       Ingredient.parse('1 2/3 cups potatoes, diced, peeled'),
     ).to.deep.equal(
